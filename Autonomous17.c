29b6faf8cc329724124b78c772b9b6826971b2d7
COBrien19 <caitlinobrien@aol.com>
2017-11-29 11:38:48 -0500

    First draft of autonomous code
    does not compile, rip

---
+++ Autonomous17.c
@@ -0,0 +1,66 @@
+//autonomous code
+
+//
+task moveWheels(int wheelPower, int curve)
+{
+
+}
+
+//power is what direction and how fast to turn
+	//if power is a positive integer it goes clockwise
+	//if power is a negative integer it goes counterclockwise
+//time is how many seconds to run the code
+task turn(int wheelPower, int time)
+{
+
+}
+
+//lifts up a yellow cone in its claw and holds it mid air
+task liftCone()
+{
+
+}
+
+//places the cone the robot is holding midair on the mobile goal in the pouch
+task placeOnMobileGoal()
+{
+
+}
+
+//lifts the mobile goal behind it and places it into the pouch
+task liftMobileGoal()
+{
+
+}
+
+task placeMobileGoal()
+{
+
+}
+
+//opens the claw that will pick up the cones we will place on the mobile goal
+//since it is the same claw as last year, for convenience I will use the same code once I get the chance
+task openClaw()
+{
+
+}
+
+//closes the claw that will pick up the cones we will place on the mobile goal
+//since it is the same claw as last year, for convenience I will use the same code once I get the chance
+task closeClaw()
+{
+
+}
+
+task main()
+{
+	//turn clockwise
+
+	//pick up cone
+	//drive clockwise forward
+	//drive backwards
+	//pick up mobile goal
+	//turn clockwise in an arc until you reach the 10 point zone
+	//wait for driver phase
+
+}
