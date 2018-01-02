fdf7186db014eb0ae0ebf5c47db049843ccec19a
jgallicchio <jamesgallicchio@gmail.com>
2018-01-02 16:57:23 -0500

    Destroy the bad files for being very bad

--- competition/config.c
+++
@@ -1,4 +0,0 @@
-#pragma config(Motor,  port2,           leftFront,     tmotorServoContinuousRotation, openLoop)
-#pragma config(Motor,  port3,           rightFront,      tmotorServoContinuousRotation, openLoop)
-#pragma config(Motor,  port4,           leftBack,    tmotorServoContinuousRotation, openLoop)
-#pragma config(Motor,  port5,           rightBack,     tmotorServoContinuousRotation, openLoop)
--- competition/main.c
+++
@@ -1,7 +0,0 @@
-#include "config.c"
-#include "movement.c"
-#include "auto.c"
-
-task main() {
-
-}
