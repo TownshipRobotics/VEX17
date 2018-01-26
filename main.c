f7bfe781a3868ac541e7908f866c7f15203f1437
COBrien19 <caitlinobrien@aol.com>
2018-01-26 13:09:30 -0500

    Comment Fixes
    Finalized carrier code

--- main.c
+++ main.c
@@ -175,14 +175,19 @@ void updateClaw()
 	//5D lowers arm
 void updateArm()
 {
-	//int power = 0;
-
-	if(vexRT[Btn5D] == 1) //if left lower Z button is pressed
+	if(vexRT[Btn5D] == 1) //if left lower z button is pressed
 	{
-		//power += 1;
-		motor[armLeft] = -8;	//-7-power;
-		motor[armRight] = 7;	//6+power;
+		motor[armLeft] = -8;
+		motor[armRight] = 7;
 	}
+	//if the potentiometer sends back 0 and I attempted to change where the the 0 point is on the robot by adding 2000
+	//it works without the 2000, but now that the position of the potentiometer is changed, it will no longer work the
+	//way it used to.
+	//SensorValue[pot] gives you the value the potentiometer is currently at
+	//if the potentiometer value is equal to or less than 0, it will be above 120 degrees from the ground and should hang
+	//when it reaches that point or neither of the L1 and L2 buttons are being pressed
+	//this has to be between the if lowering arm and if raising arm because you want to see if you want to lower it first,
+	//then check to see if it is past the point where you shouldn't raise it, then if you want to raise the arm up
 	else if(((SensorValue[pot]+2000) <= 0)||((vexRT[Btn5U] == 0) && (vexRT[Btn5D] == 0)))
 	{
 		motor[armLeft] = -30;
@@ -190,57 +195,38 @@ void updateArm()
 	}
   else if(vexRT[Btn5U] == 1) //if left upper Z button is pressed
 	{
-		//power += 60;
-		motor[armLeft] = -110;	//-50-power;
-		motor[armRight] = 110;	//50+power;
+		motor[armLeft] = -110;
+		motor[armRight] = 110;
 	}
 
 }
 
-//lifts the mobile goal, needs to be heavily tested
+//lifts the mobile goal
 void raiseCarrier()
 {
-	//move carrier motors at speed so it should difinitely be able to lift the mobile goal
-	//for(int coefficient = 1; coefficient <= 10; coefficient++)
-	//{
-	//	motor[carrierLeft] = 10*coefficient;
-	//	motor[carrierRight] = 10*coefficient;
-	//	sleep(100);
-	//}
+	//gradually increase motor speed for about 2 seconds
 	for(int base = 1; base <= 10; base++)
 	{
 		motor[carrierLeft] = pow(base,2);
 		motor[carrierRight] = -pow(base,2);
 		sleep(186);
 	}
-	//wait 1 second, needs to be tested and probably changed but this is the test value
-	//sleep(1000);
 	//stop carrier motors
 	motor[carrierLeft] = 0;
 	motor[carrierRight] = 0;
 }
 
-//lowers the mobile goal, needs to be heavily tested
-//lowers the mobile goal, needs to be heavily tested
+
+//lowers the mobile goal
 void lowerCarrier()
 {
-	//for(int coefficient = 1; coefficient <= 4; coefficient++)
-	//{
-	//	motor[carrierLeft] = -10*coefficient;
-	//	motor[carrierRight] = -10*coefficient;
-	//	sleep(250);
-	//}
+	//gradually increase motor speed for about 2 seconds
 	for(int base = 1; base <= 10; base++)
 	{
 		motor[carrierLeft] = -(pow(base,2));
 		motor[carrierRight] = (pow(base,2));
 		sleep(105);
 	}
-	//move carrier motors at the same descending speed the arm has, 6
-	//motor[carrierRight] = -50;
-	//motor[carrierLeft] = -50;
-	//wait 1 second, needs to be tested and probably changed but this is the test value
-	//sleep(1000);
 	//stop carrier motors
 	motor[carrierRight] = 0;
 	motor[carrierLeft] = 0;
